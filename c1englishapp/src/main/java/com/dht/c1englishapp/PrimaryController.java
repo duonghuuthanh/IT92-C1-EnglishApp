4729b7393b54104835bd6bd9e0d337dc036ba301
Thanh Duong <dhthanhqa@gmail.com>
2022-02-23 08:36:20 +0700

    demo conflict

--- c1englishapp/src/main/java/com/dht/c1englishapp/PrimaryController.java
+++ c1englishapp/src/main/java/com/dht/c1englishapp/PrimaryController.java
@@ -12,6 +12,9 @@ public class PrimaryController {
     }
     
     public void checkHandler(ActionEvent event) {
-        
+        int a = 20, b = 20;
+        if (a > 0) {
+            int c = a + b;
+        }
     }
 } 
